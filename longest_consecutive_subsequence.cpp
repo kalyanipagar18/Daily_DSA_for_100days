491ee63ec4ae1ef510ac7041c1c7453813ad7617
Kalyanipagar <159225439+kalyanipagar18@users.noreply.github.com>
2024-12-31 08:56:24 +0530

    Day 6
    It is easy way according to my logic

---
+++ longest_consecutive_subsequence.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+
+    // Function to return length of longest subsequence of consecutive integers.
+    int longestConsecutive(vector<int>& arr) {
+        // Your code here
+        unordered_set<int> elements(arr.begin(), arr.end());
+    int longestStreak = 0;
+
+    for (int num : arr) {
+        
+        if (elements.find(num - 1) == elements.end()) {
+            int currentNum = num;
+            int currentStreak = 1;
+
+        
+            while (elements.find(currentNum + 1) != elements.end()) {
+                currentNum++;
+                currentStreak++;
+            }
+
+        
+            longestStreak = max(longestStreak, currentStreak);
+        }
+    }
+
+    return longestStreak;
+    }
+};
