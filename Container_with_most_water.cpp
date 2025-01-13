902583b17b46a221c8f74936dfac825612da7c28
Kalyanipagar <159225439+kalyanipagar18@users.noreply.github.com>
2025-01-13 07:50:31 +0530

    Create Container_with_most_water.cpp

---
+++ Container_with_most_water.cpp
@@ -0,0 +1,26 @@
+class Solution {
+
+  public:
+    int maxWater(vector<int> &arr) {
+        // code here
+        int left = 0;                
+        int right = arr.size() - 1; 
+        int maxArea = 0;             
+        while (left < right) {
+       
+        int width = right - left;
+        int currentHeight = std::min(arr[left], arr[right]);
+        int currentArea = width * currentHeight;
+
+        maxArea = std::max(maxArea, currentArea);
+
+        if (arr[left] < arr[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return maxArea;
+
+    }
+};
