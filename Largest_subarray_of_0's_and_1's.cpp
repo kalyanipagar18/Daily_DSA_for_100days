e2b98da50a6dedfe34a91ba6235fc7387f080d7b
Kalyanipagar <159225439+kalyanipagar18@users.noreply.github.com>
2025-01-16 07:49:40 +0530

    Day 21

---
+++ Largest_subarray_of_0's_and_1's.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int maxLen(vector<int> &arr) {
+        // Your code here
+        int maxlen = 0;
+  for (int start = 0; start < n; start++) {
+    int zeroes = 0, ones = 0;
+    for (int end = start; end < n; end++) {
+      if (nums[end] == 0) {
+        zeroes++;
+      } else {
+        ones++;
+      }
+      if (zeroes == ones) {
+        maxlen = max(maxlen, end - start + 1);
+      }
+    }
+  }
+  return maxlen;
+    }
+};
