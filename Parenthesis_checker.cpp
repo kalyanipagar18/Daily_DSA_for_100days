8ef05ab79147db5f33ce13f76cee14f6b6040275
Kalyanipagar <159225439+kalyanipagar18@users.noreply.github.com>
2025-01-21 08:16:16 +0530

    Day 25

---
+++ Parenthesis_checker.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    bool isParenthesisBalanced(string& s) {
+        // code here
+        stack<char> stk;
+        unordered_map<char, char> matching = {{')', '('}, {']', '['}, {'}', '{'}};
+
+        for (char c : s) {
+          if (matching.count(c)) {  
+            if (stk.empty() || stk.top() != matching[c]) {
+                return false; 
+            }
+            stk.pop(); 
+        } else {
+            stk.push(c); 
+        }
+    }
+
+    return stk.empty();
+    }
+};
